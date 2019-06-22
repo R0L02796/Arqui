3c1a85a77604d598844fb235953e505ac16274ef
R0L02796 <alejandro.rolandelli@gmail.com>
2019-06-21 23:39:38 -0300

    me olvide el snk

--- Userland/SampleCodeModule/snakeModule.c
+++ Userland/SampleCodeModule/snakeModule.c
@@ -33,7 +33,7 @@ void printFruit(Color color, Fruit b) {
 	drawBall(color, 10, b->posX, b->posY);
 }
 
-void printFrame() {
+void printFrameSnk() {
 	drawRectangle(white, xResolution/2, 2, (xResolution/2 )-2, 0);
 	drawRectangle(white, xResolution/2, yResolution-2, (xResolution/2 )-2, 0);
 	drawRectangle(white, 2, yResolution/2, 1, (yResolution/2)-2);
