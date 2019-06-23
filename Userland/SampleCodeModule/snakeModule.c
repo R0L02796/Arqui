e0da91df70ba68019c7e3f464a36cd96d49c2b9b
R0L02796 <alejandro.rolandelli@gmail.com>
2019-06-23 18:17:25 -0300

    fsafsa

--- Userland/SampleCodeModule/snakeModule.c
+++ Userland/SampleCodeModule/snakeModule.c
@@ -263,6 +263,8 @@ void printInitScreenSnk(Fruit fruit,Snake snake) {
 }
 void printSnake(Color color, SnakePart s) {
 	SnakePart aux = s;
+	drawRectangle(color, aux->posX, aux->posY, 4, 4);
+	aux=aux->tail;
 	while (aux!=NULL)
 	{
 		drawRectangle(color, aux->posX, aux->posY, 4, 4);
